fc11793e2531109103611d9eb3d65ed3d79f6038
Mostafa Erfani <mostafa.erfani@gmail.com>
2016-11-02 21:57:58 -0400

    Show.ejs updated

--- public/script.js
+++ public/script.js
@@ -2,18 +2,18 @@ console.log("hello world");
 $(document).ready(function() {
 
   $(function() {
-      $('a[href*="#"]:not([href="#"])').click(function() {
-          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-              $('html, body').animate({
-              scrollTop: target.offset().top
-              }, 1000);
-              return false;
-              }
-          }
-      });
+      // $('a[href*="#"]:not([href="#"])').click(function() {
+      //     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      //     var target = $(this.hash);
+      //     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      //     if (target.length) {
+      //         $('html, body').animate({
+      //         scrollTop: target.offset().top
+      //         }, 1000);
+      //         return false;
+      //         }
+      //     }
+      // });
   });
 
   $('.js--waypoint-1').addClass('animated fadeInDown');
